518f1c521e2bd89a0c09413664af8082629829d8
“Jamie <jamiedevine77@gmail.com>
2023-05-10 17:23:49 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Logo from '../assets/gringalogo.png';
+import { Link } from 'react-router-dom';
+import Reorder from '../assets/reorder.png';
+
+import '../styles/Navbar.css';
+
 
 function Navbar() {
   return (
     <div className='navbar'>
-    <div className='leftSide'></div>
+    <div className='leftSide'>
     <img src={Logo} />
+    </div>
     <div className='rightSide'></div>
+        <Link to="/"> Home</Link>
+        <Link to="/menu"> Menu</Link>
+        <Link to="/about"> About</Link>
+        <Link to="/contact"> Contact</Link>
+        <button>
+        <img src={Reorder} />
+        </button>
     </div>
   )
 }
