c5d6bd550bb667e3f76858289cf2e337d90bc194
“Jamie <jamiedevine77@gmail.com>
2023-05-11 14:33:34 -0500

    homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Logo from '../assets/gringalogo.png';
 import { Link } from 'react-router-dom';
 import Reorder from '../assets/reorder.png';
-
 import '../styles/Navbar.css';
 
 
 function Navbar() {
+
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks)
+  };
+
   return (
     <div className='navbar'>
-    <div className='leftSide'>
+    <div className='leftSide' id={openLinks ? 'open' : 'close'}>
     <img src={Logo} />
-    </div>
-    <div className='rightSide'></div>
+    <div className='hiddenLinks'>
         <Link to="/"> Home</Link>
         <Link to="/menu"> Menu</Link>
         <Link to="/about"> About</Link>
         <Link to="/contact"> Contact</Link>
-        <button>
+    </div>
+    </div>
+    <div className='rightSide'>
+        <Link to="/"> Home</Link>
+        <Link to="/menu"> Menu</Link>
+        <Link to="/about"> About</Link>
+        <Link to="/contact"> Contact</Link>
+        <button onClick={toggleNavbar}>
         <img src={Reorder} />
         </button>
+        </div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import BadBunny from '../assets/bad-bunny.jpg';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>HELLOOOOOOOOO</div>
+    <div className='home'>
+      <div className='headerContainer' style={{ backgroundImage: `url(${BadBunny})` }}>
+        <h1>La Gringa Art</h1>
+        <p>let's get artsy fartsy</p>
+        <Link>
+        <button> See ART!</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
