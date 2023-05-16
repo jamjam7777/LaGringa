1acff82e9ba890a434c0c5bc714189db9165c2c2
“Jamie <jamiedevine77@gmail.com>
2023-05-16 16:43:23 -0500

    playing with framer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import BadBunny from '../assets/bad-bunny.jpg';
 import '../styles/Home.css';
+import { motion } from "framer-motion";
 
 function Home() {
   return (
@@ -10,7 +11,10 @@ function Home() {
         <h1>La Gringa Art</h1>
         <p>let's get artsy fartsy</p>
         <Link to='/art'>
+          <div className='buttonContainer'>
+          <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} /> 
         <button> See ART!</button>
+        </div>
         </Link>
       </div>
     </div>
