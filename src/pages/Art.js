42b09505032ca52710c0838cf714bae64a94a521
“Jamie <jamiedevine77@gmail.com>
2023-05-25 14:36:30 -0500

    add links

--- src/pages/Art.js
+++ src/pages/Art.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { ArtList } from "../components/ArtList";
 import ArtItem  from '../components/ArtItem';
 import "../styles/Art.css";
 
 function Art() {
+
+  const [data, setData] = useState({img: '', i: 0})
+
+    const viewImage = (ArtItem, i)=> { 
+        setData({ArtItem, i})
+    }
   return (
     <div className="art">
       <h1 className="menuTitle">Art for Sale</h1>
       <div className="menuList">
-        {ArtList.map((artItem, key) => {
+        {ArtList.map((artItem, i) => {
           return (
             
             <ArtItem 
-                    key={key}
+                    key={i}
                     image={artItem.image}
                     name={artItem.name}
                     price={artItem.price}
+                    onClick={()=>viewImage(artItem.image, i)}
                 />
                 
           );
